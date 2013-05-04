479875b8a194d2837ec431f7c69624032cb9db5f
Haley Teng <hteng@nvidia.com>
2013-05-04 18:26:35 +0800

    add header for make_haley_version.sh
    Signed-off-by: Haley Teng <hteng@nvidia.com>

--- make_haley_version.sh
+++ make_haley_version.sh
@@ -1,3 +1,9 @@
+#
+# XXX: you might need to execute --
+#          sudo apt-get build-dep vim
+#      to build successfully
+#
+
 make distclean
 export CFLAGS="-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Wformat-security -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1"
 export LDFLAGS="-Wl,-z,relro -Wl,--as-needed -ltinfo -lnsl -lattr -ldl -lpthread"
