25cb28c8bd64e85f1be983f1c6866a4d0c3e46a1
Haley Teng <hteng@nvidia.com>
2013-05-04 18:23:33 +0800

    add "--with-modified-by" for configure
    To show "Modified by ..." in 'vim --version'
    
    Signed-off-by: Haley Teng <hteng@nvidia.com>

--- make_haley_version.sh
+++ make_haley_version.sh
@@ -1,6 +1,6 @@
 make distclean
 export CFLAGS="-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Wformat-security -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1"
 export LDFLAGS="-Wl,-z,relro -Wl,--as-needed -ltinfo -lnsl -lattr -ldl -lpthread"
-./configure --with-features=huge --without-x --disable-gui --enable-pythoninterp --prefix=/usr
+./configure --with-features=huge --without-x --disable-gui --enable-pythoninterp --prefix=/usr --with-modified-by=haley.teng@gmail.com
 make
 
